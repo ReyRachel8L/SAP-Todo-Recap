7a9088ffeb059b63568e5537e442c5003b429016
ReyRachel8L <rlim.rachel08@gmail.com>
2022-07-30 09:51:54 +0800

    Initial Commit

---
+++ SAP Todo Recap/SAP_Todo_RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_Todo_RecapApp.swift
+//  SAP Todo Recap
+//
+//  Created by LIM YEE SIN RACHEL stu on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_Todo_RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
