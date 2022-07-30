e021398fe204a7db3f686cb19812db9fca2ed1ea
ReyRachel8L <rlim.rachel08@gmail.com>
2022-07-30 10:08:24 +0800

    Add struct, show in list

--- SAP Todo Recap/ContentView.swift
+++ SAP Todo Recap/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
     
     var body: some View {
         List(todoItems) { todoItem in
-            Text(todoItems.title)
+            Text(todoItem.title)
             
         }
     }
